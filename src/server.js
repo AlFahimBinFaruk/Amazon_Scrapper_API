f207083e374e7f3c2d06d14a1a93c44edecf9b7b
al fahim bin faruk <fmd03700@gmail.com>
2022-06-20 20:53:54 +0600

    cors options updated

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 //cors options
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://amazon-scrapper.netlify.app",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 //use others
